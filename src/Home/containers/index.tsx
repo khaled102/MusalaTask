0bc9bdd6427313a62847296489883f53bf02e9ef
khaled102 <khaledadel122@gmail.com>
2022-06-20 12:47:41 +0200

    feat: allow flat list to refresh data

--- src/Home/containers/index.tsx
+++ src/Home/containers/index.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import {View, FlatList} from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import {View, FlatList, RefreshControl} from 'react-native';
 import styles from './style';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { NewsCard } from './newsCard';
+import { news } from '../actions';
 
 export const HomeScreen = () =>  {
+  const dispatch = useDispatch();
   const newsData = useSelector((state: any) => state.home.newsReducer);
+  const [refresh, setRefresh] = useState<boolean>(false);
   const renderItem = ({item}: any) => (
     <NewsCard item={item} />
   );
+  const handleOnRefresh = useCallback(() => {
+    setRefresh(true);
+    dispatch(news());
+  }, []);
+  useEffect(() => {
+    setRefresh(false);
+  }, []);
   return (
     <View style={styles.container}>
       {newsData?.data &&
@@ -19,6 +29,14 @@ export const HomeScreen = () =>  {
           directionalLockEnabled
           bounces={false}
           showsVerticalScrollIndicator={false}
+          refreshControl={
+              <RefreshControl 
+                refreshing={refresh} 
+                onRefresh={handleOnRefresh}
+                tintColor="#F8852D"/>
+          }
+          // onRefresh={() => handleOnRefresh()}
+          // refreshing={refresh}
         />
       }
     </View>
