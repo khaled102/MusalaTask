e6c7c352b9ef3af57e6e45bc5803548320e3511d
khaled102 <khaledadel122@gmail.com>
2022-06-21 21:57:23 +0200

    feat: deep link open specific news by add index to deep link

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,1 +1,3 @@
 export * from './searchBar';
+export * from './customBackButton';
+
--- src/Home/containers/details/index.tsx
+++ src/Home/containers/details/index.tsx
@@ -11,24 +11,34 @@ type newsCardProps = {
 };
 export const NewsDetails = (props: newsCardProps) =>  {
   const { route } = props;
-  const { item } = route.params;
+  const { item, initalUrl } = route.params;
   const themeData = useSelector((state: any) => state.setting.themeModeReducer);
+  const newsData = useSelector((state: any) => state.home.newsReducer);
   const [date, setDate] = useState<string>('');
+  const [itemParam, setItemParam] = useState<any>(item);
   useEffect(() => {
-    const newsDate = '2022-06-20T16:11:26Z';
+    const newsDate = itemParam?.publishedAt;
     const formatedDate = moment(newsDate).format("MMM Do YY");
     setDate(formatedDate);
+    if(initalUrl) {
+      var index = initalUrl?.substring(initalUrl.indexOf("=")+1);
+      setItemParam(newsData.data.articles[index]);
+    }
   }, []);
   return (
-    <NewsCard 
-      title={item.title}
-      description={item.description}
-      imageURL={item.urlToImage}
-      author={item.author}
-      publishedAt={date}
-      theme={themeData.data}
-      content={item.content}
-      type={'fullScreen'}
-    />
+    <>
+      {itemParam &&
+        <NewsCard 
+          title={itemParam?.title}
+          description={itemParam?.description}
+          imageURL={itemParam?.urlToImage}
+          author={itemParam?.author}
+          publishedAt={date}
+          theme={themeData?.data}
+          content={itemParam?.content}
+          type={'fullScreen'}
+        />
+      }
+    </>
   );
 }
